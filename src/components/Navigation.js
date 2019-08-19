937c3f332e6335a9dc3ea619868e629107b781ef
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-19 10:56:28 -0700

    refactor

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,14 +8,11 @@ const Navigation = () => {
         <h1>React Router Mini</h1>
         <div>
           <Link to="/">Home</Link>
-          {/* <a href="">Home</a> */}
         </div>
         <div>
-          {/* <a href="">About</a> */}
           <Link to="/about">About</Link>
         </div>
         <div>
-          {/* <a href="">Contact</a> */}
           <Link to="/contact">Contact</Link>
         </div>
       </div>
